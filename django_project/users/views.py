47c15bd6b798570bb6b64e043cb09784b525e14c
Shubendu <biswasshubendu4@gmail.com>
2020-08-28 02:03:55 +0530

    profile setup

--- django_project/users/views.py
+++ django_project/users/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
-from .forms import UserRegisterForm
 from django.contrib.auth.decorators import login_required
+from .forms import UserRegisterForm
+
 
 def register(request):
     if request.method == 'POST':
@@ -9,15 +10,14 @@ def register(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Your account has been created! You can log in now {username}!')
+            messages.success(request, f'Your account has been created! You are now able to log in')
             return redirect('login')
-
     else:
         form = UserRegisterForm()
+    return render(request, 'users/register.html', {'form': form})
 
-    
-    return render(request,'users/register.html',{'form':form})
 
 @login_required
 def profile(request):
     return render(request, 'users/profile.html')
+
